b65af1174c9add860059abf73a34bfc3afb2b153
Daniel Wu <danielwuhoo@hotmail.com>
2018-07-03 23:44:11 -0400

    added bot

---
+++ index.js
@@ -0,0 +1,33 @@
+	
+const Discord = require("discord.js");
+const client = new Discord.Client();
+const fs = require("fs");
+client.config = require("./config.json");
+client.commands = new Map();
+
+fs.readdir("./commands/", (err, files) => {
+	if (err){
+		return console.error(err);
+	}
+	files.forEach(file => {
+		if (!file.endsWith(".js")) return;
+		let cmd = require(`./commands/${file}`);
+		client.commands.set(file.split(".")[0], cmd);
+
+	});
+});
+
+fs.readdir("./events/", (err, files) => {
+	if (err){
+		return console.error(err);
+	}
+	files.forEach(file => {
+		let event = require(`./events/${file}`);
+		let eventName = file.split(".")[0];
+		client.on(eventName, event.bind(null, client));
+
+	});
+
+});
+
+client.login(client.config.token);
