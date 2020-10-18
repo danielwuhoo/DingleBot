d08185c28044b6aaeeeb3e8a30b026f07cca7ba8
danielwuhoo <danielwuhoo@gmail.com>
2020-10-18 19:55:57 -0400

    fix echo for embeds

--- events/messageUpdate.js
+++ events/messageUpdate.js
@@ -2,6 +2,8 @@ module.exports = async (client, oldMessage, newMessage) => {
     if (!client.config.echoDelete) return;
 
     if (oldMessage.author.bot) return;
+    
+    if (oldMessage.content == newMessage.content) return;
 
     await oldMessage.channel.send(`${oldMessage.content}\n <@${oldMessage.author.id}>`, {files: [...oldMessage.attachments.values()]});
 }
